92e455bfd1459925533477ce58aa0414159fd5a8
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-04-25 22:46:24 +0100

    feat(aws_codestarconnections_connection): create initial connection for kwame-mintah repositories
    connection created to be used with code pipeline

--- main.tf
+++ main.tf
@@ -5,3 +5,18 @@ data "aws_availability_zones" "avaliabile_zones" {}
 # Data source to get the access to the effective Account ID, User ID, and ARN 
 # in which Terraform is authorized.
 data "aws_caller_identity" "current_caller_identity" {}
+
+locals {
+  name_prefix = "${var.project_name}-${var.aws_region}-${var.env_prefix}"
+}
+
+# GitHub CodeStar Connection 
+# Authentication with the connection provider must be completed in the AWS Console.
+resource "aws_codestarconnections_connection" "github_kwame_mintah" {
+  name          = "${local.name_prefix}-github"
+  provider_type = "GitHub"
+
+  tags = {
+    "Repository" = "https://github.com/kwame-mintah"
+  }
+}
