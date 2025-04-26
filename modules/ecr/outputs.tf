616819d97d432db51827e92d6d103a3e3d4709d1
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-04-26 16:28:16 +0100

    refactor(ecr): add additonal output to module
    output the ecr name to be used with codepipeline and iam etc

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,3 +1,11 @@
+output "ecr_repository_name" {
+  value       = aws_ecr_repository.repository.name
+  description = <<-EOF
+    The name of the repository.
+
+EOF
+}
+
 output "ecr_repository_arn" {
   value       = aws_ecr_repository.repository.arn
   description = <<-EOF
