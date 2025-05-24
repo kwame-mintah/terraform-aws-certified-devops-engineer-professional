9c48a7c10d3c3e5f69e46b5043fd6fb4650c0fa5
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-05-24 17:57:16 +0100

    feat(pipelines): invoke new cloudformation pipeline from python codepipeline
    new stage `Invoke_CloudFormation_Pipeline` added to `python_codepipeline` that will call a new pipeline `cloudformation_template_codepipeline` created to handle various cloudformation templates, accepting necassary pipeline variables to successfully run.

--- cloudformation.tf
+++ cloudformation.tf
@@ -8,13 +8,6 @@ data "http" "km_dynamodb_template" {
   url = "https://raw.githubusercontent.com/kwame-mintah/aws-cloudformation-playground/e81092ed4e6b27d887ef2dbb489bb8efa59a44f4/dynamodb-terraform-deployment-template.yaml"
 }
 
-data "http" "km_lambda_api_gateway_template" {
-  request_headers = {
-    Accept = "text/yaml"
-  }
-  url = "https://raw.githubusercontent.com/kwame-mintah/aws-cloudformation-playground/301746222955e9bf2f1c74c8177a51ba9b722c0d/lambda-api-gateway-deployment-template.yaml"
-}
-
 resource "aws_cloudformation_stack" "dynamodb_table_stack" {
   name               = "${local.name_prefix}-dynamodb-table-stack"
   timeout_in_minutes = 10
@@ -30,19 +23,106 @@ resource "aws_cloudformation_stack" "dynamodb_table_stack" {
   )
 }
 
-resource "aws_cloudformation_stack" "lambda_api_gateway_stack" {
-  name               = "${local.name_prefix}-lambda-api-gateway-stack"
-  timeout_in_minutes = 10
-  capabilities       = ["CAPABILITY_AUTO_EXPAND", "CAPABILITY_IAM"]
 
-  parameters = {
-    DockerImageUri    = "827284457226.dkr.ecr.eu-west-2.amazonaws.com/devops-engineer-aws-fastapi-lambda-api-gateway:f49c964c3acf36c2339a2cbfde78c77aa0420c2a"
-    FunctionName      = "${local.name_prefix}-fastapi-dynamodb-crud"
-    DynamoDBTableName = aws_cloudformation_stack.dynamodb_table_stack.outputs["TableName"]
-  }
-  template_body = data.http.km_lambda_api_gateway_template.response_body
+#---------------------------------------------------
+# CloudFormation IAM Role for CodePipeline
+#---------------------------------------------------
+resource "aws_iam_role" "cloudformation_provider_role" {
+  name        = "CloudFormationProviderRole"
+  description = "Role for CloudFormation provider in CodePipeline"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "codepipeline.amazonaws.com"
+        }
+      },
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudformation.amazonaws.com"
+        }
+      }
+    ]
+  })
 
   tags = merge(
     var.tags
   )
+}
+
+# Create inline policies for the role
+resource "aws_iam_role_policy" "cloudformation_provider_policy" {
+  name = "CloudFormationProviderPolicy"
+  role = aws_iam_role.cloudformation_provider_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "apigateway:DELETE",
+          "apigateway:GET",
+          "apigateway:PATCH",
+          "apigateway:POST",
+          "apigateway:PUT",
+          "apigateway:TagResource",
+          "cloudformation:CreateChangeSet",
+          "cloudformation:CreateStack",
+          "cloudformation:DeleteStack",
+          "cloudformation:DescribeStackEvents",
+          "cloudformation:DescribeStackEvents",
+          "cloudformation:DescribeStackResource",
+          "cloudformation:DescribeStackResources",
+          "cloudformation:DescribeStacks",
+          "cloudformation:GetStackPolicy",
+          "cloudformation:GetTemplate",
+          "cloudformation:UpdateStack",
+          "cloudformation:ValidateTemplate",
+          "iam:AttachRolePolicy",
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:DetachRolePolicy",
+          "iam:GetRole",
+          "iam:PassRole",
+          "iam:TagRole",
+          "lambda:AddPermission",
+          "lambda:CreateFunction",
+          "lambda:DeleteFunction",
+          "lambda:GetFunction",
+          "lambda:GetFunction",
+          "lambda:PublishVersion",
+          "lambda:RemovePermission",
+          "lambda:TagResource",
+          "lambda:UpdateFunctionCode",
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:cloudformation:*:aws:transform/Serverless-*",
+          "arn:aws:iam::${data.aws_caller_identity.current_caller_identity.account_id}:role/*",
+          "arn:aws:lambda:*:${data.aws_caller_identity.current_caller_identity.account_id}:function:*",
+          "arn:aws:cloudformation:*:${data.aws_caller_identity.current_caller_identity.account_id}:stack/*",
+          "arn:aws:iam::${data.aws_caller_identity.current_caller_identity.account_id}:role/${local.name_prefix}*",
+          "arn:aws:apigateway:*::/tags/*",
+          "arn:aws:apigateway:*::/apis*"
+        ]
+      },
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Effect = "Allow"
+        Resource = [
+          module.codepipeline_artifact_store.s3_bucket_arn,
+          "${module.codepipeline_artifact_store.s3_bucket_arn}/*"
+        ]
+      }
+    ]
+  })
 }
--- pipelines.tf
+++ pipelines.tf
@@ -18,6 +18,7 @@ resource "aws_codepipeline" "python_codepipeline" {
 
   # Stage declaration
   # https://docs.aws.amazon.com/codepipeline/latest/userguide/stage-requirements.html
+  # https://docs.aws.amazon.com/codepipeline/latest/userguide/action-reference.html
   stage {
     name = "Source"
 
@@ -91,6 +92,119 @@ resource "aws_codepipeline" "python_codepipeline" {
   }
 
   stage {
+    name = "Invoke_CloudFormation_Pipeline"
+
+    action {
+      name     = "Deploy_CloudFormation_Template"
+      category = "Invoke"
+      owner    = "AWS"
+      provider = "CodePipeline"
+      version  = "1"
+
+      configuration = {
+        PipelineName = aws_codepipeline.cloudformation_template_codepipeline.name
+        Variables = jsonencode([
+          {
+            name  = "ActionMode"
+            value = "CREATE_UPDATE"
+          },
+          {
+            name  = "StackName"
+            value = "devops-engineer-lambda-api-gateway-stack"
+          },
+          {
+            name  = "TemplatePath"
+            value = "lambda-api-gateway-deployment-template.yaml"
+          },
+          {
+            name = "ParameterOverrides"
+            value = jsonencode({
+              "DockerImageUri" : "${module.aws_fastapi_lambda_api_gateway_ecr.ecr_repository_url}:#{SourceVariables.CommitId}",
+              "FunctionName" : "${local.name_prefix}-fastapi-dynamodb-crud",
+              "DynamoDBTableName" : aws_cloudformation_stack.dynamodb_table_stack.outputs["TableName"]
+            })
+          },
+        ])
+      }
+    }
+  }
+
+  tags = merge(
+    var.tags
+  )
+
+  depends_on = [module.aws_fastapi_lambda_api_gateway_ecr, module.codepipeline_artifact_store, module.codepipeline_iam_role, aws_codepipeline.cloudformation_template_codepipeline]
+}
+
+#---------------------------------------------------
+# CloudFormation Template Deployment Pipeline
+#---------------------------------------------------
+resource "aws_codepipeline" "cloudformation_template_codepipeline" {
+  name          = "${local.name_prefix}-cloudformation-template-codepipeline"
+  pipeline_type = "V2"
+  role_arn      = module.codepipeline_iam_role.codepipeline_iam_role_arn
+
+
+  variable {
+    name        = "StackName"
+    description = "The name of an existing stack or a stack to be created."
+  }
+
+  variable {
+    name          = "ActionMode"
+    default_value = "CREATE_UPDATE"
+    description   = "The action AWS CloudFormation performs on a stack or change set."
+  }
+
+  variable {
+    name          = "Capabilities"
+    default_value = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
+    description   = "Use of Capabilities acknowledges that the template might have the capabilities to create and update some resources on its own."
+  }
+
+  variable {
+    name        = "TemplatePath"
+    description = "Represents the AWS CloudFormation template file."
+  }
+
+  variable {
+    name        = "ParameterOverrides"
+    description = "Parameters defined in the stack template and provide values for them at the time of stack creation or update."
+  }
+
+  artifact_store {
+    location = module.codepipeline_artifact_store.s3_bucket
+    type     = "S3"
+    encryption_key {
+      id   = module.codepipeline_artifact_store.s3_bucket_kms_key_arn
+      type = "KMS"
+    }
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name               = "Pull_GitHub_Repository"
+      category           = "Source"
+      owner              = "AWS"
+      provider           = "CodeStarSourceConnection"
+      version            = "1"
+      output_artifacts   = ["source_output"]
+      namespace          = "SourceVariables"
+      timeout_in_minutes = 5
+
+      configuration = {
+        ConnectionArn        = aws_codestarconnections_connection.github_kwame_mintah.arn
+        FullRepositoryId     = "kwame-mintah/aws-cloudformation-playground"
+        BranchName           = "main"
+        OutputArtifactFormat = "CODE_ZIP"
+        DetectChanges        = "false"
+      }
+    }
+  }
+
+  stage {
     name = "CloudFormation"
 
     # CloudFormation build action reference
@@ -104,15 +218,12 @@ resource "aws_codepipeline" "python_codepipeline" {
       input_artifacts = ["source_output"]
 
       configuration = {
-        ActionMode   = "CREATE_UPDATE"
-        StackName    = "${local.name_prefix}-lambda-api-gateway-stack-t"
-        TemplatePath = "https://raw.githubusercontent.com/kwame-mintah/aws-cloudformation-playground/301746222955e9bf2f1c74c8177a51ba9b722c0d/lambda-api-gateway-deployment-template.yaml"
-        Capabilities = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
-        ParameterOverrides = jsonencode({
-          "DockerImageUri" : "${module.aws_fastapi_lambda_api_gateway_ecr.ecr_repository_url}:#{SourceVariables.CommitId}",
-          "FunctionName" : "${local.name_prefix}-fastapi-dynamodb-crud-t",
-          "DynamoDBTableName" : aws_cloudformation_stack.dynamodb_table_stack.outputs["TableName"]
-        })
+        ActionMode         = "#{variables.ActionMode}"
+        StackName          = "#{variables.StackName}"
+        TemplatePath       = "source_output::#{variables.TemplatePath}"
+        Capabilities       = "#{variables.Capabilities}"
+        RoleArn            = aws_iam_role.cloudformation_provider_role.arn
+        ParameterOverrides = "#{variables.ParameterOverrides}"
       }
     }
   }
@@ -120,6 +231,4 @@ resource "aws_codepipeline" "python_codepipeline" {
   tags = merge(
     var.tags
   )
-
-  depends_on = [module.aws_fastapi_lambda_api_gateway_ecr, module.codepipeline_artifact_store, module.codepipeline_iam_role]
 }
